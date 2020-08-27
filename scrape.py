fa881f3649583d3607c818c86fbf53ccbb68659b
Dylan Wolfe <dylwolf@bgsu.edu>
2020-08-27 17:25:31 -0400

    Add funcs scrape_title and scrape_price

--- scrape.py
+++ scrape.py
@@ -23,14 +23,12 @@ def scrape_init():
 
 def scrape_title(html):
 
-	# Find all titles
+	# Find all titles on page
 	title_re = '<a href=.*?title=.*?</a.*?>' # First 'title' tag and the '/title' tag that comes after.
 	title_results = re.findall(title_re, html, re.IGNORECASE)
 
-	title_results[:] = [re.sub('<.*?title="', '', entry) for entry in title_results]
-
-	#title = match_results.group()
-	#title = re.sub("<.*?>", "", title) # Remove tags
+	title_results[:] = [re.sub('<.*?title="', "", entry) for entry in title_results] # Subtract left-side filler
+	title_results[:] = [re.sub('".*?a>', '', entry) for entry in title_results] # Subtract right-side filler
 
 	print(title_results)
 
@@ -38,9 +36,13 @@ def scrape_title(html):
 
 def scrape_price(html): # For books.toscrape.com
 
-	# Find price
+	# Find all prices on page
+	price_re = '<p class="price_color".*?</p>'
+	price_results = re.findall(price_re, html, re.IGNORECASE)
 
-	print(price)
+	price_results[:] = [re.sub('<.*?>', "", entry) for entry in price_results]
+
+	print(price_results)
 
 	return
 
@@ -49,6 +51,7 @@ def scrape_price(html): # For books.toscrape.com
 raw_html = scrape_init()
 
 scrape_title(raw_html)
+scrape_price(raw_html)
 
 
 input('Press Enter to end the program.')
