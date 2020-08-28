8fbb723f9c6911b992d97dd3a41d0f98e1892967
Dylan Wolfe <dylwolf@bgsu.edu>
2020-08-28 17:49:45 -0400

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,7 +1,7 @@
 # Simple Web Scraper
 # Author: Dylan Wolfe
 # Date: 8/25/2020
-#
+# This program is specifically made to be used with books.toscrape.com.
 
 #-- Imports --#
 import re
@@ -9,15 +9,13 @@ from urllib.request import urlopen
 
 
 #-- Functions --#
-def scrape_init():
+def scrape_init(select_num):
 
 	# Open url and read HTML
-	url = "http://books.toscrape.com/"
+	url = "http://books.toscrape.com/catalogue/page-%s.html" % select_num
 	page = urlopen(url)
 	html_encoded = page.read()
 	html = html_encoded.decode("utf-8")
-
-	#print(html)
 
 	return html
 
@@ -30,8 +28,6 @@ def scrape_title(html):
 	title_results[:] = [re.sub('<.*?title="', "", entry) for entry in title_results] # Subtract left-side filler
 	title_results[:] = [re.sub('".*?a>', '', entry) for entry in title_results] # Subtract right-side filler
 
-	#print(title_results)
-
 	return(title_results)
 
 def scrape_price(html): # For books.toscrape.com
@@ -40,22 +36,48 @@ def scrape_price(html): # For books.toscrape.com
 	price_re = '<p class="price_color".*?</p>'
 	price_results = re.findall(price_re, html, re.IGNORECASE)
 
-	price_results[:] = [re.sub('<.*?>', "", entry) for entry in price_results]
-
-	#print(price_results)
+	price_results[:] = [re.sub('<.*?>', "", entry) for entry in price_results] # Subtract tags
 
 	return(price_results)
 
+def scrape_page(select_num):
+
+	# Call func to retreive page html.
+	raw_html = scrape_init(select_num)
+
+	# Call funcs to extract titles and prices.
+	title_list = scrape_title(raw_html)
+	price_list = scrape_price(raw_html)
+
+	# Creates a dictionary that contains titles with matching prices.
+	combined_list = {title_list[entry]: price_list[entry] for entry in range(len(price_list))}
+
+	print(combined_list)
+
+	return
+
+def select_page():
+
+	# Choose page according to user input
+
+	while True:
+		select_num = input("Choose a catalogue page to scrape (1-50).")
+		try:
+			select_num = int(select_num)
+		except:
+			print("Please enter a number. (1-50)")
+			continue
+		if select_num < 1 or select_num > 50:
+			print("Please enter a number. (1-50)")
+			continue
+		break
+
+	scrape_page(select_num)
+
+	return
+
 #-- MAIN --#
 
-raw_html = scrape_init()
-
-title_list = scrape_title(raw_html)
-price_list = scrape_price(raw_html)
-
-# Creates a dictionary that contains titles with matching prices. 
-combined_list = {title_list[entry]: price_list[entry] for entry in range(len(price_list))}
-
-print(combined_list)
+select_page()
 
 input('Press Enter to end the program.')
